b1201ecedf9dd0a80200d5bcd15755f3bc37a35b
Prateek Bhatnagar <bprateek10@yahoo.in>
2024-09-07 02:16:53 +0530

    user specs refactoring

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -20,9 +20,16 @@ RSpec.describe User, type: :model do
   describe 'validations' do
     subject { build(:user) }
 
-    it { should validate_presence_of(:email) }
-    it { should validate_uniqueness_of(:email) }
-    it { should validate_presence_of(:password) }
-    it { should validate_length_of(:password).is_at_least(8) }
+    context 'email' do
+      it { should validate_presence_of(:email) }
+      it { should validate_uniqueness_of(:email) }
+      it { should allow_value('test@yopmail.com').for(:email) }
+      it { should_not allow_value('test').for(:email) }
+    end
+
+    context 'password' do
+      it { should validate_presence_of(:password) }
+      it { should validate_length_of(:password).is_at_least(8) }
+    end
   end
 end
